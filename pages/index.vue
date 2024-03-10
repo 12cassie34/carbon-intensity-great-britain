48b608857bc66675b76fe919ed6b3c54dbd824f6
12cassie34 <ineslin35@gmail.com>
2024-03-10 15:34:54 +0000

    feat: add the today's intensity chart

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang='ts'>
 import { fetchData, formatBarChartData, type IntensityData } from '~/utlis'
 
+const todayData = await fetchData('https://api.carbonintensity.org.uk/intensity/date')
 const currentHalfHourData: { data: IntensityData[] } = await fetchData('https://api.carbonintensity.org.uk/intensity')
 const past24HoursData = await fetchData(`https://api.carbonintensity.org.uk/intensity/${new Date().toISOString()}/pt24h`)
 const forecast24HoursData = await fetchData(`https://api.carbonintensity.org.uk/intensity/${new Date().toISOString()}/fw24h`)
@@ -9,6 +10,11 @@ const forecast24HoursData = await fetchData(`https://api.carbonintensity.org.uk/
 <template>
     <div class='grid grid-cols-1 gap-4 lg:grid-cols-2'>
         <ConditionCard title='Past Half Hour' :cardData='currentHalfHourData.data[0]' />
+        <Card title='Today Intensity'>
+            <template #content>
+                <BarChart :svgId="'today-svg'" :chartData='formatBarChartData(todayData.data)'/>
+            </template>
+        </Card>
         <Card title='Past 24 Hours'>
             <template #content>
                 <BarChart :svgId="'past-24-hours-svg'" :chartData='formatBarChartData(past24HoursData.data)'/>
