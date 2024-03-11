732ae845a06a39f7113091765b214ad90771bad9
12cassie34 <ineslin35@gmail.com>
2024-03-11 13:35:21 +0000

    fix: enable specificDateData be reactive when it's passed into BarChart

--- pages/search-historical-data.vue
+++ pages/search-historical-data.vue
@@ -7,16 +7,17 @@ const specificDate = ref(new Date())
 const updateDate = (date: Date) => specificDate.value = date
 const initialSpecificDateData = await fetchData(`https://api.carbonintensity.org.uk/intensity/date/${format(specificDate.value, 'yyyy-MMMM-dd')}`)
 
-const specificDateData = ref<{ data: IntensityData[] }>(initialSpecificDateData)
+const specificDateData = ref<{ data: IntensityData[] } | null>(initialSpecificDateData)
 
 watch(specificDate, async () => {
+    specificDateData.value = null
     specificDateData.value = await fetchData(`https://api.carbonintensity.org.uk/intensity/date/${format(specificDate.value, 'yyyy-MMMM-dd')}`)
 })
 </script>
 
 <template>
     <DatePickerDropdown :date='specificDate' :handleUpdateDate="updateDate" class="mb-4" />
-    <Card :title='`Carbon Intensity in ${format(specificDate, FULL_DATE_FORMAT)}`' :key="specificDate.valueOf()">
+    <Card :title='`Carbon Intensity in ${format(specificDate, FULL_DATE_FORMAT)}`' :key="specificDate.valueOf()" v-if="!!specificDateData">
         <template #content>
             <BarChart :svgId="'specific-date-svg'" :chart-data='formatBarChartData(specificDateData.data)' />
         </template>
